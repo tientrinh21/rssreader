e987bfcb555cdc0c30dc8e99de16b77ff8e838a4
tientrinh21 <tqtien.a11619@gmail.com>
2024-02-16 18:48:07 +0900

    Fix Login dialog layout

--- src/components/dialog-login.tsx
+++ src/components/dialog-login.tsx
@@ -1,47 +1,47 @@
 import { Button } from '@/components/ui/button'
 import {
-	Dialog,
-	DialogContent,
-	DialogDescription,
-	DialogFooter,
-	DialogHeader,
-	DialogTitle,
-	DialogTrigger,
-	DialogClose,
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose,
 } from '@/components/ui/dialog'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
 export function DialogLogin() {
-	return (
-		<Dialog>
-			<text asChild>
-				<Button>Login</Button>
-			</text>
-			<DialogContent className="sm:max-w-[425px]">
-				<DialogHeader>
-					<DialogTitle>Login</DialogTitle>
-					<DialogDescription>
-						Input your API Key here.
-					</DialogDescription>
-				</DialogHeader>
-				<div className="grid gap-4 py-4">
-					<div className="grid grid-cols-4 items-center gap-4">
-						<Label htmlFor="apikey" className="text-right">
-							API Key
-						</Label>
-						<Input id="apikey" className="col-span-3" />
-					</div>
-				</div>
-				<DialogFooter>
-					<DialogClose asChild>
-						<Button type="button" variant="outline">
-							Close
-						</Button>
-					</DialogClose>
-					<Button type="submit">Next</Button>
-				</DialogFooter>
-			</DialogContent>
-		</Dialog>
-	)
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>Login</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Login</DialogTitle>
+          <DialogDescription>
+            Input your API Key here.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="apikey" className="text-right">
+              API Key
+            </Label>
+            <Input id="apikey" className="col-span-3" />
+          </div>
+        </div>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button type="button" variant="outline">
+              Close
+            </Button>
+          </DialogClose>
+          <Button type="submit">Next</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  )
 }
